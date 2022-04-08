c3b9f7e93b18eec9ecfad2ef36dd2478872d9f48
Dayron <corderodhc@gmail.com>
2022-04-08 12:27:26 -0400

    FirstApp

---
+++ Controllers/TruckController.cs
@@ -0,0 +1,107 @@
+﻿using Data.Interface;
+using FirstAPP.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstAPP.Controllers
+{
+    public class TruckController : Controller
+    {
+        private readonly ITruck _truck;
+        public TruckController(ITruck truck)
+        {
+            _truck = truck;
+        }
+
+        // GET: Truck
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+        // GET: Truck/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Truck/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Truck truck)
+        {
+            
+                if (ModelState.IsValid)
+                {
+                    _truck.AddTruck(truck);
+                    return RedirectToAction("Details");
+                }                
+            
+                return RedirectToAction("Create");
+            
+        }
+
+        // GET: Truck/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var model = _truck.Get(id);
+            return View(model);
+        }
+
+        // POST: Truck/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Truck truck)
+        {
+            try
+            {
+                _truck.UpdateTruck(truck);
+                return RedirectToAction("ShowAll");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Truck/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var model = _truck.Get(id);
+            return View(model);
+        }
+
+        // POST: Truck/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Truck truck)
+        {
+            try
+            {
+                _truck.DeleteTruck(truck);
+                return RedirectToAction("ShowAll");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult ShowAll()
+        {
+            var model = _truck.GetAll();
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var result = _truck.Get(id);
+            return View(result);
+        }
+    }
+}
---
+++ Controllers/TruckDriverController.cs
@@ -0,0 +1,94 @@
+﻿using Data.Servicies;
+using FirstAPP.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstAPP.Controllers
+{
+    public class TruckDriverController : Controller
+    {
+        private readonly ITruckDriver _truckDrivers;
+        public TruckDriverController(ITruckDriver truckDriver)
+        {
+            _truckDrivers = truckDriver;
+        }
+        // GET: TruckDriverController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: TruckDriverController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TruckDriverController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TruckDriverController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TruckDriverController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TruckDriverController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TruckDriverController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TruckDriverController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
